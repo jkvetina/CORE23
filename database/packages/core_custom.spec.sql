44d7ee6172618cf27bf0ff1b2fcdd9d628a7ecde
Jan Kvetina <jan.kvetina@gmail.com>
2024-12-13 15:11:06 +0100

    Simplify log handling

--- database/packages/core_custom.spec.sql
+++ database/packages/core_custom.spec.sql
@@ -7,57 +7,23 @@ AS
 
 
 
-    --
-    -- There are several logging functions/procedures in the CORE package,
-    --
-    -- core.log_error       -- for errors, obviously
-    -- core.log_warning     -- for warnings
-    -- core.log_debug       -- for less important messages
-    -- core.log_request     -- for url parameters
-    -- core.log_module      -- to mark start of a procedure/function
-    -- core.log_success     -- to mark successful end of the module
-    --
-    -- the default destination was the APEX_DEBUG_MESSAGES, so this package
-    -- will allow you to switch to Logger or your own logging thingy...
-    --
-
-    PROCEDURE log_error (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    );
-
-
-
-    PROCEDURE log_warning (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    );
-
-
-
-    PROCEDURE log_debug (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    );
-
-
-
-    PROCEDURE log_module (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    );
+    FUNCTION log__ (
+        in_type                 CHAR,
+        in_message              VARCHAR2,
+        in_arguments            VARCHAR2,
+        in_payload              CLOB        := NULL,
+        in_context_id           NUMBER      := NULL,
+        --
+        in_app_id               NUMBER      := NULL,
+        in_page_id              NUMBER      := NULL,
+        in_user_id              VARCHAR2    := NULL,
+        in_session_id           NUMBER      := NULL,
+        --
+        in_caller               VARCHAR2    := NULL,
+        in_backtrace            VARCHAR2    := NULL,
+        in_callstack            VARCHAR2    := NULL
+    )
+    RETURN NUMBER;
 
 END;
 /
--- database/packages/core_custom.sql
+++ database/packages/core_custom.sql
@@ -5,87 +5,70 @@ CREATE OR REPLACE PACKAGE BODY core_custom AS
     AS
     BEGIN
         -- extract env name (cloud edition)
-        RETURN REGEXP_REPLACE(SYS_CONTEXT('USERENV', 'DB_NAME'), '^[^_]*_', '');
+        RETURN REPLACE(
+            REGEXP_REPLACE(SYS_CONTEXT('USERENV', 'DB_NAME'), '^[^_]*_', ''),
+            'DYR', '');
     END;
 
 
 
-    PROCEDURE log_error (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
+    FUNCTION log__ (
+        in_type                 CHAR,
+        in_message              VARCHAR2,
+        in_arguments            VARCHAR2,
+        in_payload              CLOB        := NULL,
+        in_context_id           NUMBER      := NULL,
+        --
+        in_app_id               NUMBER      := NULL,
+        in_page_id              NUMBER      := NULL,
+        in_user_id              VARCHAR2    := NULL,
+        in_session_id           NUMBER      := NULL,
+        --
+        in_caller               VARCHAR2    := NULL,
+        in_backtrace            VARCHAR2    := NULL,
+        in_callstack            VARCHAR2    := NULL
     )
+    RETURN NUMBER
     AS
     BEGIN
-        APEX_DEBUG.ERROR (
-            p_message       => in_message || '|' || in_arguments || '|' || in_payload || in_backtrace || in_callstack,
-            p_max_length    => 32767
-        );
-    END;
-
-
-
-    PROCEDURE log_warning (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    )
-    AS
-    BEGIN
-        --APEX_DEBUG.WARN (
-        APEX_DEBUG.MESSAGE (
-            p_message       => in_message || '|' || in_arguments || '|' || in_payload || in_backtrace || in_callstack,
-            p_max_length    => 32767,
-            p_force         => TRUE
-        );
-    END;
-
-
-
-    PROCEDURE log_debug (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    )
-    AS
-    BEGIN
-        APEX_DEBUG.MESSAGE (
-            p_message       => in_message || '|' || in_arguments || '|' || in_payload,
-            p_max_length    => 32767,
-            p_force         => TRUE
-        );
-        /*
-        logger.log (
-            p_text    => in_message || ' LEN:' || LENGTH(in_payload),
-            p_scope   => '',
-            p_extra   => in_payload
-            --p_params  in tab_param default logger.gc_empty_tab_param);
-        );
-        */
-    END;
-
-
-
-    PROCEDURE log_module (
-        in_message          VARCHAR2,
-        in_arguments        VARCHAR2,
-        in_payload          VARCHAR2,
-        in_backtrace        VARCHAR2,
-        in_callstack        VARCHAR2
-    )
-    AS
-    BEGIN
-        APEX_DEBUG.MESSAGE (
-            p_message       => in_message || '|' || in_arguments || '|' || in_payload || in_backtrace || in_callstack,
-            p_max_length    => 32767,
-            p_force         => TRUE
-        );
+        CASE in_type
+            WHEN 'E' THEN
+                APEX_DEBUG.ERROR (
+                    p_message       => in_message || '|' || in_arguments || '|' || in_payload || in_backtrace || in_callstack,
+                    p_max_length    => 32767
+                );
+            WHEN 'W' THEN
+                --APEX_DEBUG.WARN (
+                APEX_DEBUG.MESSAGE (
+                    p_message       => in_message || '|' || in_arguments || '|' || in_payload || in_backtrace || in_callstack,
+                    p_max_length    => 32767,
+                    p_force         => TRUE
+                );
+            ELSE
+                APEX_DEBUG.MESSAGE (
+                    p_message       => in_message || '|' || in_arguments || '|' || in_payload,
+                    p_max_length    => 32767,
+                    p_level         => 4,
+                                    -- 1 = c_log_level_error            critical error
+                                    -- 2 = c_log_level_warn             less critical error
+                                    -- 4 = c_log_level_info constant    default level if debugging is enabled (for example, used by apex_application.debug)
+                                    -- 5 = c_log_level_app_enter        application: messages when procedures/functions are entered
+                                    -- 6 = c_log_level_app_trace        application: other messages within procedures/functions
+                                    -- 8 = c_log_level_engine_enter     APEX engine: messages when procedures/functions are entered
+                                    -- 9 = c_log_level_engine_trace     APEX engine: other messages within procedures/functions
+                    p_force         => TRUE
+                );
+                /*
+                logger.log (
+                    p_text    => in_message || ' LEN:' || LENGTH(in_payload),
+                    p_scope   => '',
+                    p_extra   => in_payload
+                    --p_params  in tab_param default logger.gc_empty_tab_param);
+                );
+                */
+            END CASE;
+        --
+        RETURN APEX_DEBUG.GET_LAST_MESSAGE_ID();
     END;
 
 END;
