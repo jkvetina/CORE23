5d29cbab0a6f25badbad0d883904c9c9c1ce6094
Jan Kvetina <jan.kvetina@gmail.com>
2024-12-14 13:46:19 +0100

    Add objects status

--- database/SETUP.sql
+++ database/SETUP.sql
@@ -65,17 +65,24 @@ SELECT
         LISTAGG(g.privilege, ', ') WITHIN GROUP (ORDER BY g.privilege),
         'ALTER, DEBUG, DELETE, FLASHBACK, INDEX, INSERT, ON COMMIT REFRESH, QUERY REWRITE, READ, REFERENCES, SELECT, UPDATE', 'ALL'
         ) AS privileges,
-    g.grantable
+    --
+    g.grantable,
+    o.status
     --
 FROM user_synonyms s
 LEFT JOIN user_tab_privs_recd g
     ON g.owner          = s.table_owner
     AND g.table_name    = s.table_name
+LEFT JOIN all_objects o
+    ON o.owner          = s.table_owner
+    AND o.object_name   = s.table_name
+    AND o.object_type   = g.type
 GROUP BY
     s.synonym_name,
     s.table_owner,
     s.table_name,
     g.type,
-    g.grantable
+    g.grantable,
+    o.status
 ORDER BY 1;
 
