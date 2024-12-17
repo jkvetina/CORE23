3b154bd2c1388aff3ff772023c2a8b3fddbe80a0
Jan Kvetina <jan.kvetina@gmail.com>
2024-12-17 20:15:03 +0100

    Cleanup

--- database/packages/core_custom.sql
+++ database/packages/core_custom.sql
@@ -32,70 +32,78 @@ CREATE OR REPLACE PACKAGE BODY core_custom AS
     AS
         PRAGMA AUTONOMOUS_TRANSACTION;
         --
-        v_level     PLS_INTEGER;
-        v_flag      VARCHAR2(16);
+        v_message       VARCHAR2(32767);
     BEGIN
+        -- prepare message
+        v_message := CASE
+            WHEN in_type IN (core.flag_error, core.flag_warning)
+                THEN in_message
+                    || CASE WHEN in_arguments IS NOT NULL THEN CHR(10) || '^ARGS: '         || in_arguments END
+                    || CASE WHEN in_backtrace IS NOT NULL THEN CHR(10) || '^BACKTRACE: '    || in_backtrace END
+                --
+            ELSE
+                in_message
+                    || CASE in_type
+                        WHEN core.flag_start    THEN ' [START] '
+                        WHEN core.flag_end      THEN ' [END] '
+                        END
+                    || CASE WHEN in_arguments IS NOT NULL THEN CHR(10) || '^ARGS: '         || in_arguments END
+            END;
+
+        -- actually log the message into apex_debug_messages view
         CASE in_type
             WHEN core.flag_error THEN
                 APEX_DEBUG.ERROR (
-                    p_message       => in_message
-                                        || CASE WHEN in_arguments IS NOT NULL THEN CHR(10) || '^ARGS: '         || in_arguments END
-                                        || CASE WHEN in_backtrace IS NOT NULL THEN CHR(10) || '^BACKTRACE: '    || in_backtrace END,
+                    p_message       => v_message,
                     p_max_length    => 32767
                 );
                 --
             WHEN core.flag_warning THEN
                 --APEX_DEBUG.WARN (
                 APEX_DEBUG.MESSAGE (
-                    p_message       => in_message
-                                        || CASE WHEN in_arguments IS NOT NULL THEN CHR(10) || '^ARGS: '         || in_arguments END,
+                    p_message       => v_message,
                     p_level         => 2,
                     p_max_length    => 32767,
                     p_force         => TRUE
                 );
-            ELSE
-                -- prepare proper flags
-                CASE in_type
-                    WHEN core.flag_start THEN
-                        v_level := 4;
-                        v_flag  := ' [START] ';
-                    WHEN core.flag_end THEN
-                        v_level := 4;
-                        v_flag  := ' [END] ';
-                    ELSE
-                        v_level := 6;
-                        v_flag  := '';
-                    END CASE;
                 --
+            ELSE
                 APEX_DEBUG.MESSAGE (
-                    p_message       => in_message || v_flag
-                                        || CASE WHEN in_arguments IS NOT NULL THEN CHR(10) || '^ARGS: '         || in_arguments END,
+                    p_message       => v_message,
                     p_max_length    => 32767,
-                    p_level         => v_level,
-                                    -- 1 = c_log_level_error            critical error
-                                    -- 2 = c_log_level_warn             less critical error
-                                    -- 4 = c_log_level_info constant    default level if debugging is enabled (for example, used by apex_application.debug)
-                                    -- 5 = c_log_level_app_enter        application: messages when procedures/functions are entered
-                                    -- 6 = c_log_level_app_trace        application: other messages within procedures/functions
-                                    -- 8 = c_log_level_engine_enter     APEX engine: messages when procedures/functions are entered
-                                    -- 9 = c_log_level_engine_trace     APEX engine: other messages within procedures/functions
+                    p_level         => CASE in_type
+                                            WHEN core.flag_start    THEN 4
+                                            WHEN core.flag_end      THEN 4
+                                            ELSE 6
+                                            END,
+                                            -- 1 = c_log_level_error            critical error
+                                            -- 2 = c_log_level_warn             less critical error
+                                            -- 4 = c_log_level_info constant    default level if debugging is enabled (for example, used by apex_application.debug)
+                                            -- 5 = c_log_level_app_enter        application: messages when procedures/functions are entered
+                                            -- 6 = c_log_level_app_trace        application: other messages within procedures/functions
+                                            -- 8 = c_log_level_engine_enter     APEX engine: messages when procedures/functions are entered
+                                            -- 9 = c_log_level_engine_trace     APEX engine: other messages within procedures/functions
                     p_force         => TRUE
                 );
-                --
-                /*
-                logger.log (
-                    p_text    => in_message
-                                        || CASE WHEN in_arguments IS NOT NULL THEN CHR(10) || '^ARGS: '         || in_arguments END,
-                    p_scope   => ''
-                    --p_extra   => in_payload
-                    --p_params  in tab_param default logger.gc_empty_tab_param
-                );
-                */
             END CASE;
-        --
+
+            -- you can use Logger or anything you want instead
+            /*
+            logger.log (
+                p_text    => v_message,
+                p_scope   => ''
+                p_extra   => in_payload
+            );
+            */
+
+        -- also show in console
+        DBMS_OUTPUT.PUT_LINE(v_message);
+
+        -- finish transaction
         COMMIT;
         --
         RETURN APEX_DEBUG.GET_LAST_MESSAGE_ID();
+        --
     EXCEPTION
     WHEN OTHERS THEN
         ROLLBACK;
