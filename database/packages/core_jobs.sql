bdb5f13d691c5391083fa910732b47a6434ef0b7
Jan Kvetina <jan.kvetina@gmail.com>
2025-08-17 12:49:49 +0200

    Add synonyms with grants check

--- database/packages/core_jobs.sql
+++ database/packages/core_jobs.sql
@@ -490,6 +490,48 @@ CREATE OR REPLACE PACKAGE BODY core_jobs AS
             core.raise_error();
         END;
 
+        -- append content
+        BEGIN
+            OPEN v_cursor FOR
+                SELECT
+                    s.synonym_name,
+                    g.type              AS object_type,
+                    s.table_owner       AS owner,
+                    s.table_name        AS object_name,
+                    --
+                    REPLACE (
+                        LISTAGG(g.privilege, ', ') WITHIN GROUP (ORDER BY g.privilege),
+                        'ALTER, DEBUG, DELETE, FLASHBACK, INDEX, INSERT, ON COMMIT REFRESH, QUERY REWRITE, READ, REFERENCES, SELECT, UPDATE', 'ALL'
+                        ) AS privileges,
+                    --
+                    CASE WHEN g.grantable = 'YES' THEN 'Y' END AS is_grantable,
+                    --
+                    o.status,
+                    CASE WHEN o.status != 'VALID' THEN 'RED' END AS status__style
+                    --
+                FROM user_synonyms s
+                LEFT JOIN user_tab_privs_recd g
+                    ON g.owner          = s.table_owner
+                    AND g.table_name    = s.table_name
+                LEFT JOIN all_objects o
+                    ON o.owner          = s.table_owner
+                    AND o.object_name   = s.table_name
+                    AND o.object_type   = g.type
+                GROUP BY
+                    s.synonym_name,
+                    s.table_owner,
+                    s.table_name,
+                    g.type,
+                    g.grantable,
+                    o.status
+                ORDER BY 1;
+            --
+            v_out := v_out || get_content(v_cursor, 'Synonyms');
+        EXCEPTION
+        WHEN OTHERS THEN
+            core.raise_error();
+        END;
+
         -- send mail to all developers
         send_mail (
             in_recipients   => in_recipients,
