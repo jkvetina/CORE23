2ff8cb1ece82bc2b65519240a104803641417247
Jan Kvetina <jan.kvetina@gmail.com>
2026-04-08 11:46:53 +0200

    Add CORE_LOCKSMITH_ENABLE scheduler job
    Creates a DBMS_SCHEDULER job that re-enables the CORE_LOCKSMITH trigger every 5 minutes, supporting the locksmith enable/disable workflow.

---
+++ database/jobs/core_locksmith_enable.sql
@@ -0,0 +1,38 @@
+DECLARE
+    in_job_name             CONSTANT VARCHAR2(128)  := 'CORE_LOCKSMITH_ENABLE';
+    in_run_immediatelly     CONSTANT BOOLEAN        := FALSE;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--');
+    DBMS_OUTPUT.PUT_LINE('-- REPLACE JOB ' || UPPER(in_job_name));
+    DBMS_OUTPUT.PUT_LINE('--');
+    --
+    BEGIN
+        DBMS_SCHEDULER.DROP_JOB(in_job_name, TRUE);
+    EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+    END;
+    --
+    DBMS_SCHEDULER.CREATE_JOB (
+        job_name            => in_job_name,
+        job_type            => 'PLSQL_BLOCK',
+        job_action          => 'EXECUTE IMMEDIATE ''ALTER TRIGGER CORE_LOCKSMITH ENABLE'';',
+        number_of_arguments => 0,
+        start_date          => SYSDATE,
+        repeat_interval     => 'FREQ=MINUTELY; INTERVAL=5',
+        enabled             => FALSE,
+        auto_drop           => TRUE,
+        comments            => NULL
+    );
+    --
+    DBMS_SCHEDULER.SET_ATTRIBUTE(in_job_name, 'JOB_PRIORITY', 3);
+    DBMS_SCHEDULER.ENABLE(in_job_name);
+    COMMIT;
+    --
+    IF in_run_immediatelly THEN
+        DBMS_SCHEDULER.RUN_JOB(in_job_name);
+        COMMIT;
+    END IF;
+END;
+/
+
